a8b7056465e69426de89876ef99dd86ba0a77f6a
OLaita <oscarlaitaro@gmail.com>
2022-03-03 19:44:38 +0100

    v0.21

--- src/app/ocr/ocr.component.ts
+++ src/app/ocr/ocr.component.ts
@@ -12,6 +12,7 @@ export class OcrComponent implements OnInit {
 
   ocrResult = 'Recognizing...';
   title = 'tesseract.js-angular-app';
+  rec = false;
 
   constructor() {
     //this.doOCR();
@@ -20,7 +21,8 @@ export class OcrComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  test2(){
+  ocr(){
+    this.rec = true;
     console.log("pepsi");
     Tesseract
     .recognize('https://tesseract.projectnaptha.com/img/eng_bw.png')
@@ -30,32 +32,5 @@ export class OcrComponent implements OnInit {
     })
     .catch(console.error);
   }
-
-  /*async doOCR() {
-    const worker = createWorker({
-      logger: m => console.log(m),
-    });
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://jeroen.github.io/images/testocr.png');
-    this.ocrResult = text;
-    console.log(text);
-    await worker.terminate();
-  }*/
-
-  /*test(){
-
-    console.log("patata");
-
-    Tesseract.recognize(
-      'https://tesseract.projectnaptha.com/img/eng_bw.png',
-      'eng',
-      { logger: m => console.log(m) }
-    ).then(({ data: { text } }) => {
-      console.log(text);
-    })
-
-  }*/
 
 }

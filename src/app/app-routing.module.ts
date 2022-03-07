877ce6f7b44615436c98402bb4cce8e4e6ae7d4f
OLaita <oscarlaitaro@gmail.com>
2022-03-07 20:18:10 +0100

    v0.8.5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,30 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OcrComponent } from './ocr/ocr.component';
 import { ColorTComponent } from './color-t/color-t.component';
+import { DashboardModule } from './features/dashboard/dashboard.module';
+import { canActivate, AuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['dashboard']);
 
 const routes: Routes = [
   {path: 'ocr', component: OcrComponent},
-  {path: 'imgT', component: ColorTComponent}
+  {path: 'imgT', component: ColorTComponent},
+  { path: 'dashboard',
+    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
+    ...canActivate(redirectUnauthorizedToLogin),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+      ...canActivate(redirectLoggedInToHome),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
